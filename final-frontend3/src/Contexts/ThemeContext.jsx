65521924a4a3e558c891d4bcbc20d5cc4c643b71
Lucas Tobias Jimenez Cuestas <ltjc1008@gmail.com>
2023-06-30 21:28:19 -0300

    localstorage theme

--- final-frontend3/src/Contexts/ThemeContext.jsx
+++ final-frontend3/src/Contexts/ThemeContext.jsx
@@ -3,12 +3,18 @@ import { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 function ThemeContextProvider(props){
-    const [theme, setTheme] = useState("dark")
+    const [theme, setTheme] = useState(localStorage.getItem("theme"))
 
-    function toggleTheme() {
-        setTheme((prevTheme)=>(prevTheme === "light" ? "dark" : "light"))
-    }
+    // function toggleTheme() {
+    //     setTheme((prevTheme)=>(prevTheme === "light" ? "dark" : "light"))
+    // }
     
+    function toggleTheme() {
+        const newValue = theme === "light" ? "dark" : "light"
+        setTheme((newValue))
+        localStorage.setItem("theme", newValue)
+    }
+
     const themeValues = {
         value: theme,
         changeTheme: toggleTheme,
